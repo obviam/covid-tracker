9bc42a361936e818bb4b9044ba39f56bc27eb21c
Tamas <tamas@obviam.net>
2020-04-01 22:53:56 +0100

    Add parameter to the country page

--- src/app/coutries/country/country.page.ts
+++ src/app/coutries/country/country.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryPage implements OnInit {
 
-  constructor() { }
+  countryCode: string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.countryCode = this.route.snapshot.paramMap.get('code');
+    console.log(`Country: ${this.countryCode}`);
+    // process country data
   }
 
 }
